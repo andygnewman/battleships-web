d54b58f130028c540967bc91b3d4184ba4376197
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-07 08:40:37 +0000

    fixed bug by adding sessions. Cucumber passes

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 class BattleShips < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "..", "views") }
+  enable :sessions
 
   get '/' do
     erb :index
@@ -13,8 +14,8 @@ class BattleShips < Sinatra::Base
   end
 
   get '/registered1' do
-    @player_name1 = params[:player_name]
-    if @player_name1.empty?
+    session[:player_name1] = params[:player_name]
+    if session[:player_name1].empty?
       erb :player1
     else
       erb :player2
@@ -22,11 +23,11 @@ class BattleShips < Sinatra::Base
   end
 
   get '/registered2' do
-    @player_name2 = params[:player_name]
-    if @player_name2.empty?
+    session[:player_name2] = params[:player_name]
+    if session[:player_name2].empty?
       erb :player2
     else
-      "Thank you #{@player_name2}, you will be playing against #{@player_name1}"
+      "Thank you #{session[:player_name2]}, you will be playing against #{session[:player_name1]}"
     end
   end
 
