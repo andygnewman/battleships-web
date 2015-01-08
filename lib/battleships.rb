270e761bc0ae5327dd394cdca350ecb63046ee26
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-08 16:47:56 +0000

    further refactoring to work with new code and remove objects from sessions

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,18 +5,24 @@ require_relative 'player'
 require_relative 'board'
 require_relative 'cell'
 require_relative 'ship'
+require_relative 'fleet'
 
 class BattleShips < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "..", "views") }
   enable :sessions
+  game = Game.new
+  board1 = Board.new
+  board2 = Board.new
+  #fleets = [Fleet.new.ship_array, Fleet.new.ship_array]
+  fleet1 = Fleet.new.ship_array
+  fleet2 = Fleet.new.ship_array
 
   get '/' do
     erb :index
   end
 
   get '/new_game' do
-    game = Game.new
     session[:players] = []
     session[:fleets] = []
     erb :player_reg_form
@@ -29,10 +35,7 @@ class BattleShips < Sinatra::Base
       player = Player.new
       player.name = params[:player_name]
       session[:players] << player.name
-      player.board = Board.new(Cell)
-      fleet = [Ship.aircraft_carrier, Ship.battleship, Ship.destroyer, Ship.submarine, Ship.patrol_boat ]
-      #session[:fleets] << player.fleet.ship_array
-      # puts session.inspect
+      # player.board = Board.new(Cell)
       if session[:players].size ==2
          erb :register_complete
       else
@@ -42,19 +45,20 @@ class BattleShips < Sinatra::Base
   end
 
   get '/set_up_fleet' do
-    session[:fleet] = session[:fleets].shift
     redirect '/get_coordinates'
   end
 
   get '/get_coordinates/?:error?' do
-    if session[:fleet].size > 0
-      session[:ship] = session[:fleet].shift
+    puts fleet1
+    fleet1.empty? ? fleet = fleet2 : fleet = fleet1
+    if fleet.length >= 1
+      session[:ship] = fleet.shift
       @ship = session[:ship]
       @error = params[:error]
       erb :get_coordinates, locals: {ship: @ship, error: @error}
     else
       "You have placed all your ships"
-      redirect '/set_up_fleet' if session[:fleets] < 0
+      redirect '/set_up_fleet' if fleet == 0
     end
   end
 
@@ -62,7 +66,7 @@ class BattleShips < Sinatra::Base
     if params
       start_cell = params[:start_cell]
       orientation = params[:orientation]
-      player = ObjectSpace._id2ref(session[:players][0])
+      player = session[:players][0]
       ship = session[:ship]
       begin
         player.board.place_ship(ship, start_cell.to_sym, orientation.to_sym)
--- lib/fleet.rb
+++ lib/fleet.rb
@@ -8,9 +8,4 @@ class Fleet
     @ship_array = [Ship.aircraft_carrier, Ship.battleship, Ship.destroyer, Ship.destroyer, Ship.submarine, Ship.patrol_boat, Ship.patrol_boat, Ship.patrol_boat, Ship.patrol_boat]
   end
 
-
-  def sunk?
-    ship_array.each { |ship| return false if !ship.sunk? }
-  end
-
 end
